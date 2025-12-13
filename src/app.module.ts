cedd3e70cb3c6764f347dd1bdd173f71b5e82fb3
baothaihcmut <thaibao22042k4@gmail.com>
2025-12-13 15:45:02 +0700

    auth controller impl

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CommonModule } from './common/common.module';
 import { AuthModule } from './auth/auth.module';
@@ -9,8 +8,15 @@ import { TasksModule } from './tasks/tasks.module';
 import { MailModule } from './mail/mail.module';
 
 @Module({
-  imports: [UsersModule, CommonModule, AuthModule, ProjectsModule, TasksModule, MailModule],
+  imports: [
+    UsersModule,
+    CommonModule,
+    AuthModule,
+    ProjectsModule,
+    TasksModule,
+    MailModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
